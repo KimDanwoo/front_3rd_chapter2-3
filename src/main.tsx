5abef241642bc2fea77930c7157e05e4bc6944d6
KimDanwoo <dudgh9198@naver.com>
2025-01-22 17:54:08 +0900

    ✨ feat: main 파일 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
+import App from '@app/App'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { QueryProvider } from '@app/providers/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryProvider>
-      <App />
-    </QueryProvider>
+    <App />
   </StrictMode>,
 )
--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,7 +1,30 @@
-import PostManager from '@widgets/post/ui/PostManager'
+import { AddCommentDialog, UpdateCommentDialog } from '@widgets/comment/ui'
+import { AddPostDialog, DetailPostDialog, PostSection, UpdatePostDialog } from '@widgets/post/ui'
+import { Card } from '@shared/ui'
+import { PostCardHeader } from '@features/post/ui'
+import { UserDetailDialog } from '@widgets/user/ui'
+import { Footer, Header } from '@widgets/common'
 
-const PostPage = () => {
-  return <PostManager />
+export const PostPage = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow container mx-auto px-4 py-8">
+        <Card.Container className="w-full max-w-6xl mx-auto">
+          <PostCardHeader />
+
+          <PostSection />
+
+          <AddCommentDialog />
+          <AddPostDialog />
+          <DetailPostDialog />
+          <UpdateCommentDialog />
+          <UpdatePostDialog />
+          <UserDetailDialog />
+        </Card.Container>
+      </main>
+
+      <Footer />
+    </div>
+  )
 }
-
-export default PostPage
